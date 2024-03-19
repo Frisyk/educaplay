1d933e96c7502e3083fe381294ff7254d253e0c8
frisyk <frisnadi1@gmail.com>
2024-03-19 14:40:47 +0700

    update

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-const Card = ({ nama, gambar }) => {
+interface CardProps {
+  nama: string;
+  gambar: string;
+}
+
+const Card = ({ nama, gambar }: CardProps) => {
   return (
     <div className="bg-blue-500 flex items-end justify-between p-4 py-10 rounded-lg">
       <h2 className="text-2xl font-bold text-white">{nama}</h2>
