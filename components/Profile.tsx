a9008347a48c6664595cea7f679d468613184c5d
frisyk <frisnadi1@gmail.com>
2024-04-03 23:27:00 +0700

    fixing type bugs

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -2,26 +2,32 @@ import { getCurrentUser } from '@/lib/session';
 import Link from 'next/link';
 import ButtonLogout from './ButtonLogout';
 import Image from 'next/image';
+import profile from '../public/unknown.png'
+
 
 export default async function Profile() {
-    const user = await getCurrentUser()
+  const user = await getCurrentUser();
+  const profileImage = typeof user?.image === 'string' ? user?.image : profile;
+
   return (
     <main className="flex items-center justify-center md:h-screen">
-    <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-      <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-        <div className="w-32 text-white md:w-36">
-          <h1>EducaPlay</h1>
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <h1>EducaPlay</h1>
+          </div>
         </div>
+        <ButtonLogout />
+        <h1>{user?.name}</h1>
+        {typeof user?.image === 'string' && (
+          <Image
+            src={profileImage}
+            alt={user.name || 'Profile Image'}
+            width={400}
+            height={400}
+          />
+        )}
       </div>
-      <ButtonLogout/>
-      <h1>{user?.name}</h1>
-      <Image
-        src={user?.image}
-        alt={user?.name}
-        width={400}
-        height={400}
-      />
-    </div>
-  </main>
-  )
+    </main>
+  );
 }
