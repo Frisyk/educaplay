033e823bcc704c79a73c702d8eb45aace47d7eaa
frisyk <frisnadi1@gmail.com>
2024-04-30 10:33:28 +0700

    edit ui

--- app/page.tsx
+++ app/page.tsx
@@ -16,24 +16,25 @@ console.log(session);
   }
   
   return (
-    <main className="w-full min-h-screen pb-10 text-blue-950 bg-white flex items-center flex-col ">
+    <main className="w-full h-screen pb-10 text-blue-950 bg-white flex items-center flex-col ">
       {/* <h1 className="font-semibold text-2xl text-wrap uppercase">
         Educaplay
       </h1> */}
       <Image
         src={hero}
         alt="hero picture"
-        width={800}
-        height={1200}
+        width={1000}
+        height={1000}
+        className="h-3/4 object-cover"
       />
       <div className="rounded-3xl bg-white flex items-center flex-col mt-[-50px] z-20">
-      <p className="text-3xl p-12 font-bold leading-[45px]">
+      <p className="text-2xl p-12 font-bold text-blue-800 leading-[30px]">
       Belajar Agama <br /> dengan Asyik dan Menyenangkan <br /> Yuk
       </p>
-        <Link className="p-5 py-7 mb-10 bg-blue-800 text-white w-4/5 text-center text-xl font-bold rounded-xl flex gap-5 items-center justify-center " href={"/dashboard"}>
+        <Link className="p-3 py-5 mb-5 bg-blue-800 text-white w-4/5 text-center text-xl rounded-xl" href={"/dashboard"}>
           Menuju Halaman Utama
         </Link>
-        <button onClick={() => signIn('google')} className="p-5 bg-blue-50 text-blue-800 outline w-4/5 text-center text-xl rounded-xl flex gap-5 items-center justify-center " >
+        <button onClick={() => signIn('google')} className="p-4 bg-blue-50 text-blue-800 outline w-4/5 text-center text-xl rounded-xl flex gap-5 items-center justify-center " >
           <Icon/>
           Login with Google
         </button>
--- public/google.tsx
+++ public/google.tsx
@@ -4,8 +4,8 @@ function Icon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="40"
-      height="40"
+      width="25"
+      height="25"
       version="1.1"
       viewBox="0 0 512 512"
       xmlSpace="preserve"
