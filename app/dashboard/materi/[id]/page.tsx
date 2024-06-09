011e2492abea06045efe4f952e0c88b384ec9032
frisyk <frisnadi1@gmail.com>
2024-06-09 16:39:51 +0700

    minor

--- app/dashboard/materi/[id]/page.tsx
+++ app/dashboard/materi/[id]/page.tsx
@@ -22,18 +22,25 @@ async function MateriPage({ params }: { params: MateriParams }) {
   let imageSource: string | null | StaticImageData | undefined = post?.image
   const materi = await getAllPosts()
 
+  function boldMultipleWords(text: string, words:string[]) {
+    // Membuat ekspresi reguler untuk semua kata yang ingin ditebalkan
+    const regex = new RegExp(`(${words.join('|')})`, 'gi');
+    return text.replace(regex, '<b>$1</b>');
+  }
+
+  const content = boldMultipleWords(`${post?.content}` || "", ["istilah", "bahasa", "wajib"])
+
   if(post?.id == "7") {
     imageSource = perbedaan
   }
-  
-
   return (
+    
     <main className="min-h-screen py-20 flex  md:w-[60%] mx-auto flex-col items-center">
       <TopNavigation materi={materi} id={`/dashboard/materi/${params.id}`} />
       <div className=" mx-auto  flex flex-col text-blue-800 items-center">
         <h1 className="text-3xl font-bold my-5 text-center rounded-2xl text-blue-900 bg-blue-100 p-3">{post?.title}</h1>
         <div className="text-base leading-8 mt-3 text-justify whitespace-pre-line">
-          <p>{post?.content}</p><br />
+          <p dangerouslySetInnerHTML={{ __html: content }}></p><br />
 
             <Image
               src={imageSource || defaultImg}
