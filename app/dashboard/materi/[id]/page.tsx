28fcef11b2032af75d9c7a01c4e1996501c90e5b
frisyk <frisnadi1@gmail.com>
2024-06-09 17:33:28 +0700

    minor

--- app/dashboard/materi/[id]/page.tsx
+++ app/dashboard/materi/[id]/page.tsx
@@ -22,34 +22,28 @@ async function MateriPage({ params }: { params: MateriParams }) {
   let imageSource: string | null | StaticImageData | undefined = post?.image
   const materi = await getAllPosts()
 
-  function boldMultipleWords(text: string, words:string[]) {
-    // Membuat ekspresi reguler untuk semua kata yang ingin ditebalkan
-    const regex = new RegExp(`(${words.join('|')})`, 'gi');
-    return text.replace(regex, '<b>$1</b>');
-  }
-
-  const content = boldMultipleWords(`${post?.content}` || "", ["istilah", "bahasa", "wajib"])
-
   if(post?.id == "7") {
     imageSource = perbedaan
-  }
+  }  
+  
   return (
-    
     <main className="min-h-screen py-20 flex  md:w-[60%] mx-auto flex-col items-center">
       <TopNavigation materi={materi} id={`/dashboard/materi/${params.id}`} />
       <div className=" mx-auto  flex flex-col text-blue-800 items-center">
         <h1 className="text-3xl font-bold my-5 text-center rounded-2xl text-blue-900 bg-blue-100 p-3">{post?.title}</h1>
         <div className="text-base leading-8 mt-3 text-justify whitespace-pre-line">
-          <p dangerouslySetInnerHTML={{ __html: content }}></p><br />
-
+          {
+            post?.image || post?.id == "7" && 
             <Image
-              src={imageSource || defaultImg}
-              alt={post?.title || "details"}
-              width={1400}
-              height={1400}
-              className="min-w-20"
-              loading="lazy"
+            src={imageSource || defaultImg}
+            alt={post?.title || "details"}
+            width={1400}
+            height={1400}
+            className="min-w-20"
+            loading="lazy"
             />
+            }
+          <div dangerouslySetInnerHTML={{ __html: post?.content || "" }}></div><br />
           
         </div>
       </div>
